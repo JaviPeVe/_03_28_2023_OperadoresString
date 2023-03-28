1bc231de34360ec68401e29bc4126e76df303632
JaviPeVe <javiermail.p@gmail.com>
2023-03-28 21:16:43 +0200

    Proyecto a las 21:01

---
+++ src/com/hedima/presentacion/OperadoresLogicos.java
@@ -0,0 +1,37 @@
+package com.hedima.presentacion;
+
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        //Operadores de Comparación
+        //Devuelve un valor Booleano
+
+        int a=10;
+        int b=20;
+        boolean resultado = a>b;
+        System.out.println(resultado);
+        resultado = a<b;
+        System.out.println(resultado);
+    int c=10;
+    int d=10;
+    resultado = c == d;
+    System.out.println(resultado);
+    resultado = c!= d;
+    System.out.println(resultado);
+
+    //Operadores Lógicos
+        //and &&
+        //or ||
+        //not!
+        a=20;
+        b=10;
+        c=30;
+        resultado =(a>b) && (a<c);
+        System.out.println("and -->"+resultado);
+        resultado =(a>b) || (a<c);
+        System.out.println(resultado);
+        resultado =!(a>b);
+        System.out.println(resultado);
+
+
+    }
+}
---
+++ src/com/hedima/presentacion/ProbarClaseString.java
@@ -0,0 +1,11 @@
+package com.hedima.presentacion;
+
+public class ProbarClaseString {
+    public static void main(String[] args) {
+        String nombre = "Juan";
+        String apellido = "Perez";
+        char letra = 'a';
+        System.out.println(nombre+ " "+apellido);
+        System.out.println(nombre.concat( " "+apellido));
+    }
+}
